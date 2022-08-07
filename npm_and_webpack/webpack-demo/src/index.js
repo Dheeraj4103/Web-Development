04f98efddaafcddc1be3ac60d3a7fc143876df20
unknown <dheerajjadhav4455@gmail.com>
2022-08-07 19:18:02 +0530

    loaders and plugins added, other devdependencies added

--- npm_and_webpack/webpack-demo/src/index.js
+++ npm_and_webpack/webpack-demo/src/index.js
@@ -1,11 +1,20 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
+import Icon from './hoisting.png';
 
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // adding Image to our div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
 
-document.body.appendChild(component());
+export default document.body.appendChild(component());
--- npm_and_webpack/webpack-demo/webpack.config.js
+++ npm_and_webpack/webpack-demo/webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
+    mode: "production",
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js)$/,
+                use: "babel-loader",
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin()],
 };
