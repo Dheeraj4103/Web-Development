0cf0e0ef50ce1e64f758f1342cd035843bb7b0de
Dheeraj4103 <dheerajjadhav4455@gmail.com>
2022-10-25 11:33:43 +0530

    Search feature inserted in FindGif

--- APIs/cat.js
+++ APIs/cat.js
@@ -1,22 +1,27 @@
 const img = document.querySelector("img");
-const input = document.getElementById("search-img");
+const input = document.getElementById("ipt");
 const search = document.getElementById("search");
 
-search.addEventListener('click', (e) => {
-    console.log(input[0]);
-    e.preventDefault();
-})
-// search.onclick = function(){
-//     input.textContent = "Hello";
-// }
+var url =
+  "https://api.giphy.com/v1/gifs/translate?api_key=5EhAavGYHG0pwkhMRZD9RMlJnHCCQloc&s=cats";
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=5EhAavGYHG0pwkhMRZD9RMlJnHCCQloc&s=cats",
-  { mode: "cors" }
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+search.addEventListener("click", (e) => {
+  console.log("Submitted");
+  img.innerHTML = "";
+  url = url + input.value;
+  GetCats(url);
+  e.preventDefault();
+});
+
+function GetCats(url) {
+  fetch(url, { mode: "cors" })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
